4761f3abc2e9af34500c89b959bb88964321a226
Dancer2009 <daniloMoura26@hotmail.com>
2022-09-20 08:10:20 -0300

    Add files via upload

---
+++ AzureAdOauth.cs
@@ -0,0 +1,23 @@
+namespace WebApi_Atualizado
+{
+    public class AzureAdOauth
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+
+    public class mfadto
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string scope { get; set; }
+        public string session_state { get; set; }
+
+    }
+}
